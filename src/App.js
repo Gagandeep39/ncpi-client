fcd94f0545cbe0f76044214bdcbb0b6b49ed8081
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-24 19:58:30 +0000

    Added axios, store

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Balance } from './components/Balance';
+import Home from './components/Home';
 import Login from './components/Login';
 import { Transaction } from './components/Transaction';
 
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
           <Route exact path="/balance" component={Balance} />
           <Route exact path="/transaction" component={Transaction} />
         </Switch>
