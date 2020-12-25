f5bd5bac5ccabb7725c0be804c9b81f8f0c85aed
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-25 06:15:30 +0000

    Added auth header

---
+++ src/helpers/auth-header.js
@@ -0,0 +1,9 @@
+export default function authHeader() {
+  // return authorization header with jwt token
+  const token = localStorage.getItem('token');
+  if (token) {
+    return `Bearer ${token}`;
+  } else {
+    return '';
+  }
+}
