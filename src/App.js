4c4a02de80c05bf03da696bfc376b99015b1fdec
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-25 06:12:24 +0000

    Added token and default url, header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Balance } from './components/Balance';
 import Home from './components/Home';
 import Login from './components/Login';
 import { Transaction } from './components/Transaction';
+import axios from 'axios';
+import authHeader from './helpers/auth-header';
+
+axios.defaults.baseURL = process.env.REACT_APP_URL;
+axios.defaults.headers.common['Authorization'] = authHeader();
 
 function App() {
   return (
